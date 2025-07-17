47bdd30d9702fb07dca44d1a6e0a30e2cf5d3144
Samuel <sam3bett@gmail.com>
2025-07-17 15:37:56 +0300

    initial commit

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,25 @@
 // Code EyesOnMe Component Here
+import React from 'react';
+
+function EyesOnMe() {
+  // Handler for when the button receives focus
+  const handleFocus = () => {
+    console.log('Good!');
+  };
+
+  // Handler for when the button loses focus
+  const handleBlur = () => {
+    console.log('Hey! Eyes on me!');
+  };
+
+  return (
+    <button onFocus={handleFocus} onBlur={handleBlur}>
+      Eyes on me
+    </button>
+  );
+}
+
+export default EyesOnMe;
+
+
+
